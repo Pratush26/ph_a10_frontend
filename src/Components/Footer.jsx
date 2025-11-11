2b877d9e20d66a8c54e4efc78d8f7478f6ce1cd5
Pratush <pratushbarua26@gmail.com>
2025-11-11 11:00:51 +0600

    fixed footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 import '../Utils/utility.css'
 import Logo from '/logo.svg'
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
     return (
@@ -12,15 +14,15 @@ export default function Footer() {
                 </div>
                 <div className="text-gray-400 flex flex-col gap-3 text-sm">
                     <h6 className="text-lg font-semibold text-white">Quick Links</h6>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>All Foods</Link>
+                    <Link className="hover:text-gray-200 trnsition" to='/all-foods'>All Foods</Link>
                     <Link className="hover:text-gray-200 trnsition" to='/'>Home</Link>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>Login</Link>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>Register</Link>
+                    <Link className="hover:text-gray-200 trnsition" to='/login'>Login</Link>
+                    <Link className="hover:text-gray-200 trnsition" to='/register'>Register</Link>
                 </div>
                 <div className="text-gray-400 flex flex-col gap-3 text-sm">
                     <h6 className="text-lg font-semibold text-white">Features</h6>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>Add Foods</Link>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>My Foods</Link>
+                    <Link className="hover:text-gray-200 trnsition" to='/add-food'>Add Foods</Link>
+                    <Link className="hover:text-gray-200 trnsition" to='/my-foods'>My Foods</Link>
                 </div>
                 <div className="text-gray-400 flex flex-col gap-3 text-sm">
                     <h6 className="text-lg font-semibold text-white">Contact & Support</h6>
@@ -28,11 +30,13 @@ export default function Footer() {
                     <Link className="hover:text-gray-200 trnsition" to='/'>+880 123 456 789</Link>
                     <Link className="hover:text-gray-200 trnsition" to='/'>123 Commerce Street, Dhaka, Bangladesh</Link>
                 </div>
-                <div className="text-gray-400 space-x-3 space-y-2 text-sm">
+                <div className="text-gray-400 space-x-3 space-y-2 text-xl">
                     <h6 className="text-lg font-semibold text-white">Social Links</h6>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>support@Smartdeals.com</Link>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>+880 123 456 789</Link>
-                    <Link className="hover:text-gray-200 trnsition" to='/'>123 Commerce Street, Dhaka, Bangladesh</Link>
+                    <div className="flex gap-3">
+                    <a className="hover:text-gray-200 trnsition" href="" target="_blank"><FaFacebookSquare /></a>
+                    <a className="hover:text-gray-200 trnsition" href="" target="_blank"><FaSquareXTwitter /></a>
+                    <a className="hover:text-gray-200 trnsition" href="" target="_blank"><FaInstagramSquare /></a>
+                    </div>
                 </div>
             </footer>
             <p className="text-center font-semibold text-xs text-gray-400 py-8">Copyright &copy; 2025 - All right reserved by Plate Share</p>
